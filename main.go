70e2c56ac51647912f5a2f3d3173187178333b5c
Keith King <KingKeithC@gmail.com>
2020-06-01 22:59:49 -0600

    Corrected Sudoku board, and finished solver.

--- main.go
+++ main.go
@@ -13,15 +13,15 @@ type Sudoku struct {
 // NewPremade returns a premade known solvable board of Sudoku.
 func NewPremade() *Sudoku {
 	board := [][]int{
-		{0, 1, 0, 0, 4, 0, 6, 0, 0},
-		{2, 7, 8, 0, 0, 3, 9, 0, 0},
-		{3, 0, 4, 0, 0, 1, 2, 0, 0},
-		{0, 0, 9, 0, 6, 0, 1, 0, 4},
-		{1, 0, 0, 4, 0, 8, 0, 0, 6},
-		{6, 0, 7, 0, 3, 0, 5, 0, 0},
-		{0, 0, 3, 5, 0, 0, 8, 0, 9},
-		{0, 0, 1, 3, 0, 0, 5, 0, 0},
-		{0, 0, 6, 0, 2, 0, 0, 1, 0},
+		{1, 2, 0, 0, 0, 0, 0, 4, 0},
+		{0, 0, 8, 1, 0, 0, 0, 0, 5},
+		{0, 4, 0, 2, 0, 9, 1, 6, 3},
+		{0, 0, 2, 6, 5, 0, 4, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{8, 0, 6, 0, 7, 1, 5, 0, 0},
+		{5, 9, 4, 7, 0, 3, 0, 8, 0},
+		{6, 0, 0, 0, 0, 5, 3, 0, 0},
+		{0, 1, 0, 0, 0, 0, 0, 5, 9},
 	}
 
 	return &Sudoku{
@@ -63,12 +63,69 @@ func (s *Sudoku) String() string {
 	return b.String()
 }
 
+// isValid takes a board, a position, and a guess and returns whether it is valid.
+// The guess should not already be in the board.
+func (s *Sudoku) isValid(row, col, guess int) bool {
+	// Search each col in the row for the guess.
+	for c := 0; c < len(s.board[row]); c++ {
+		if s.board[row][c] == guess {
+			return false
+		}
+	}
+
+	// Search each row in the col for the guess.
+	for r := 0; r < len(s.board); r++ {
+		if s.board[r][col] == guess {
+			return false
+		}
+	}
+
+	// Search the subregion for the guess.
+	subregionRow := row - row%3
+	subregionCol := col - col%3
+	for r := subregionRow; r < subregionRow+3; r++ {
+		for c := subregionCol; c < subregionCol+3; c++ {
+			if s.board[r][c] == guess {
+				return false
+			}
+		}
+	}
+
+	// no guess found, this guess is valid
+	return true
+}
+
+// Solve replaces missing numbers in the Sudoku board.
+func (s *Sudoku) Solve() bool {
+	for row := 0; row < 9; row++ {
+		for col := 0; col < 9; col++ {
+			if s.board[row][col] == 0 {
+				for guess := 1; guess <= 9; guess++ {
+					if s.isValid(row, col, guess) {
+						s.board[row][col] = guess
+						if s.Solve() {
+							return true
+						}
+						s.board[row][col] = 0
+					}
+				}
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func main() {
 	s := NewPremade()
 
-	fmt.Println("Initial Board Configuration:")
+	fmt.Println("Initial Board:")
 	fmt.Println("----------------------------")
-	fmt.Println(s.String())
-	fmt.Printf("-----------------------------\n\n")
+	fmt.Println(s)
+	fmt.Printf("----------------------------\n\n")
 
+	fmt.Printf("Solvable: %t\n", s.Solve())
+	fmt.Println("----------------------------")
+	fmt.Println(s)
+	fmt.Printf("----------------------------\n\n")
 }
